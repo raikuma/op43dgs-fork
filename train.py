519097c334f2e80b141f360ebd902a8faa5f6c8b
Woongoh Cho <whdnddh@naver.com>
2024-12-27 18:22:15 +0900

    fix

--- train.py
+++ train.py
@@ -93,7 +93,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
             image, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"]
 
             # Loss
-            gt_image = viewpoint_cam.original_image.cuda()
+            gt_image = viewpoint_image
             Ll1 = l1_loss(image, gt_image)
             loss = (1.0 - opt.lambda_dssim) * Ll1 + opt.lambda_dssim * (1.0 - ssim(image, gt_image))
             loss.backward()
